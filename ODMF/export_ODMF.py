ba94c46583345460bec7546a972a1770a2f52e17
HGarre <helene@garre.eu>
2025-10-07 16:17:24 +0200

    credentials from config cleaned

--- ODMF/export_ODMF.py
+++ ODMF/export_ODMF.py
@@ -10,15 +10,6 @@ import configparser
 from odmfclient import login
 
 config_path = "config.ini"
-
-if not os.path.exists(config_path):
-    raise FileNotFoundError(f"Config file not found: {config_path}")
-
-config = configparser.ConfigParser()
-config.read(config_path)
-
-
-# --- Read configuration ---
 config = configparser.ConfigParser()
 config.read(config_path)
 
@@ -26,27 +17,7 @@ url = config["odmf"]["url"]
 username = config["odmf"]["username"]
 password = config["odmf"]["password"]
 
-# --- ODMF API login and data retrieval ---
 with login(url, username, password) as api:
     datasets_soil_m = api.dataset.list(valuetype=10)
     data_soil_m = api.dataset.values_parquet(dsid=3146)
-
-    # Example: print dataset info
-    print("Datasets:", datasets_soil_m[:3])  # show first 3 for preview
-    print("Data loaded for dsid=12345")
-
-
-
-# # -*- coding: utf-8 -*-
-# """
-# Created on Mon Oct  6 16:25:49 2025
-#
-# @author: garre
-# """
-#
-# from odmfclient import login
-#
-# with login("https://data.fb09.uni-giessen.de/gbh/", "helene.garre", "") as api:
-#     datasets_soil_m = api.dataset.list(valuetype=10)
-#     data_soil_m = api.dataset.values_parquet(dsid=3146)
-
+    print(data_soil_m.head())
