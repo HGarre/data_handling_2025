3576ca8329f98bdc2c8363bc9ad9e28d107fb158
HGarre <helene@garre.eu>
2026-05-30 15:58:03 +0200

    Added comments and ODMF import via API (untested)

--- Campbell/convert_campbell.py
+++ Campbell/convert_campbell.py
@@ -1,11 +1,34 @@
+"""
+Created on  May 25 2026
+
+@author: Helene Garre
+
+This Skript can be used to import data downloaded from Campbell Scientific data loggers into the ODMF. 
+It cleanes unneccessary columns and converts the data into a long format with columns "time", "dataset_id", and "value"
+as required for ODMF record import. The mapping of datalogger channel names to dataset IDs is done using an Excel file. 
+The resulting DataFrame can be saved as a CSV file or directly imported into ODMF using the API.
+"""
+
 import pandas as pd
 import os
+import yaml
+from odmfclient import login
 
-project_dir = os.path.abspath(os.path.dirname(__file__))
-T2_data_path = os.path.join(project_dir, 'CR300Series_Minutentabelle.dat')
-datasetmap_path = os.path.join(project_dir, 'LED_radiation_sensors.xlsx')
 
-def convert_campbell_LED_to_ODMF_record(datalogger, data_path, starttime, endtime, datasetmap):
+def convert_campbell_LED_to_ODMF_record(data_path, datasetmap, datalogger, starttime, endtime):
+    '''
+    Convert Campbell Scientific LED voltage data to a long format suitable for ODMF.
+
+    Parameters:
+    - data_path: Path to the data as downloaded from Campbell Scientific data logger (e.g., "CR300Series_Minutentabelle.dat").
+    - datasetmap: Path to the Excel file containing the mapping of channel names to dataset IDs (e.g., "LED_radiation_sensors.xlsx").
+    - datalogger: The name of the datalogger as in the datasetmap (e.g., "T2").
+    - starttime: The start time for filtering the data (e.g. after ssetup of sensors is finished).
+    - endtime: The end time for filtering the data (e.g. before next sensor adjustment).
+    
+    Returns:    
+    - A pandas DataFrame with colums "time", "dataset_id", and "value" as required for ODMF record import. 
+    '''
 
     data = pd.read_csv(data_path, sep=',', header=0, skiprows=[0, 2, 3], na_values="NAN")
     LEDvolt_cols = [f"SEVolt_Avg({i})" for i in range(1, 13)]
@@ -30,6 +53,26 @@ def convert_campbell_LED_to_ODMF_record(datalogger, data_path, starttime, endtim
     return LEDvolt_long
 
 if __name__ == "__main__":
+
+    project_dir = os.path.abspath(os.path.dirname(__file__))
+    T2_data_path = os.path.join(project_dir, 'CR300Series_Minutentabelle.dat')
+    datasetmap_path = os.path.join(project_dir, 'LED_radiation_sensors.xlsx')
+    main_dir = os.path.join(project_dir, "../ODMF")
+    
+    config_path = os.path.join(main_dir, "config.yaml")
+    with open(config_path, "r", encoding="utf-8") as cf:
+        cfg = yaml.safe_load(cf)
+
+    odmf_cfg = cfg.get("odmf", cfg)
+
+    url = odmf_cfg["url"]
+    username = odmf_cfg["username"]
+    password = odmf_cfg["password"]
+
     T2_LED_log =convert_campbell_LED_to_ODMF_record(datalogger = "T2", data_path = T2_data_path, starttime = "2026-05-14 10:00:00", endtime = "2026-05-21 12:00:00", datasetmap = datasetmap_path)
 
     T2_LED_log.to_csv(os.path.join(project_dir, 'T2_LED_log.csv'), index=False)
+    '''
+    with login('https://path/to/odmf', 'user', 'password') as api:
+        api.dataset.add_records_parquet(T2_LED_long)
+    '''
